4b30df5cd8f7884e6566ab3f4802255d8846ce80
Isaiah Raya <rayaisaiah@gmail.com>
2024-01-21 17:16:17 -0800

    Added base site details/icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png'
+import { SocialIcon } from 'react-social-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Isaiah's Portfolio
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <img src={logo} className="App-logo" alt="Logo" />
+        <h1>
+          Isaiah Amadeus Raya
+        </h1>
+        <h2>
+          Software Engineer | Microsoft
+        </h2>
+        <div class="container">
+          <div class="social-icon"> <SocialIcon url="https://www.linkedin.com/in/isaiah-raya-0235a0208/" style={{ height: 150, width: 150 }} fgColor="currentColor" /></div>
+          <div class="social-icon"> <SocialIcon url="https://github.com/rayaisaiah" style={{ height: 150, width: 150 }} fgColor="currentColor" /></div>
+          <div class="social-icon"> <SocialIcon url="mailto:rayaisaiah@gmail.com" style={{ height: 150, width: 150 }} fgColor="currentColor" /></div>
+        </div>
       </header>
     </div>
   );
